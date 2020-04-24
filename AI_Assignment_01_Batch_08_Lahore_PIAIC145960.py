c7dfb6ecfd3935ce8dc39f4ede88d67dc48ae76c
In2ruder <64050797+In2ruder@users.noreply.github.com>
2020-04-25 00:45:58 +0400

    Update AI_Assignment_01_Batch_08_Lahore_PIAIC145960.py

--- AI_Assignment_01_Batch_08_Lahore_PIAIC145960.py
+++ AI_Assignment_01_Batch_08_Lahore_PIAIC145960.py
@@ -19,12 +19,18 @@ print(f"You have enter {second_number} as 2nd number ")
 
 multiplication_of_numbers = first_number * second_number
 addition_of_numbers = first_number + second_number
-subtraction_of_numbers = first_number - second_number
-devision_of_numbers_1st_devide_2nd = first_number/second_number
-devision_of_numbers_2nd_devide_1st = second_number/first_number
+subtraction_first_numbers_from_second = second_number - second_number
+subtraction_second_numbers_from_first = first_number - second_number
 
 print(f"Multiplication of First Number: {first_number} with Second Number: {second_number} is = {multiplication_of_numbers}")
 print(f"Addition of First Number: {first_number} with Second Number: {second_number} is = {addition_of_numbers}")
-print(subtraction_of_numbers)
-print(devision_of_numbers_1st_devide_2nd)
-print(devision_of_numbers_2nd_devide_1st)
+print(f"Subtraction of First Number :{first_number} from Second Number :{second_number} is = ",subtraction_first_numbers_from_second)
+print(f"Subtraction of Second Number :{second_number} from First Number :{first_number} is = ",subtraction_second_numbers_from_first)
+
+if (first_number != 0) and (second_number != 0):
+    devide_1st_by_2nd = first_number/second_number
+    devide_2nd_by_1st = second_number/first_number
+    print(f"Devide First Number :{first_number} by Second Number :{second_number} is =",devide_1st_by_2nd )
+    print(f"Devide Second Number :{second_number} by First Number :{first_number}is =", devide_2nd_by_1st )
+else:
+    print("Any of entered number is 0 Zero so we cannot divide any number by Zero or if we devide zero by any number it will also ZERO ")
